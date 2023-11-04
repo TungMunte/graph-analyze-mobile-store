6bfe3aaad02fb17525f11411df517b2d4222a8d6
Tung NGUYEN QUANG 322CD <seeyouagain14012000@gmail.com>
2023-11-04 10:01:10 +0200

    second commit

---
+++ project bd2/coupon.sql
@@ -0,0 +1,10 @@
+insert into coupon (date_start, date_end, reduce) values ('21-Apr-2023', '23-Apr-2024', 100);
+insert into coupon (date_start, date_end, reduce) values ('20-Dec-2022', '10-Oct-2024', 400);
+insert into coupon (date_start, date_end, reduce) values ('11-Nov-2022', '12-Jul-2024', 300);
+insert into coupon (date_start, date_end, reduce) values ('25-Feb-2023', '22-Oct-2023', 200);
+insert into coupon (date_start, date_end, reduce) values ('06-Aug-2023', '06-Dec-2023', 200);
+insert into coupon (date_start, date_end, reduce) values ('27-Sep-2023', '02-Aug-2024', 100);
+insert into coupon (date_start, date_end, reduce) values ('13-Sep-2023', '09-Oct-2024', 300);
+insert into coupon (date_start, date_end, reduce) values ('19-Dec-2022', '18-Dec-2023', 400);
+insert into coupon (date_start, date_end, reduce) values ('27-Aug-2023', '16-Aug-2024', 400);
+insert into coupon (date_start, date_end, reduce) values ('11-Jun-2023', '08-Feb-2024', 300);
